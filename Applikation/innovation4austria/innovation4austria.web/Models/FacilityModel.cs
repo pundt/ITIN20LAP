a582c1c3a78c6741f16d68f64dc04a4d24f42d50
franzPilgerstorfer <franz.pilgerstorfer@pundt.at>
2017-02-28 16:24:59 +0100

    added icons for facilities added script to insert icons in db starting useCase for filtering

---
+++ Applikation/innovation4austria/innovation4austria.web/Models/FacilityModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace innovation4austria.web.Models
+{
+    public class FacilityModel
+    {
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Applikation/innovation4austria/innovation4austria.web/Models/RoomFilterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace innovation4austria.web.Models
+{
+    public class RoomFilterModel
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        public string Start { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public string End { get; set; }
+
+        public List<FacilityModel> Facilities{ get; set; }
+    }
+}
